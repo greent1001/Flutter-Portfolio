9f9ca7924796f0b1d253df9047e248c2155f7919
Takella Green <tkgreen1001@gmail.com>
2021-05-17 18:38:09 -0700

    Flutter Portfolio

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart' show AppBar, BuildContext, Center, MaterialApp, Scaffold, StatelessWidget, Text, Widget, runApp;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
